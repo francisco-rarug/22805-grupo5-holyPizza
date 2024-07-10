904139c5cacb46c8cc3c863d77c1bf3d2064c5a4
mauriciocrojas <mauriciocc.rojas@gmail.com>
2024-07-09 22:51:46 -0300

    LoginOk-CredencialesEnLoginJs

---
+++ Js/login.js
@@ -0,0 +1,20 @@
+const users = [
+    { username: "LioMessi", password: "1234" },
+    { username: "DibuMartinez", password: "1234" },
+    { username: "FideoDiMaria", password: "1234" }
+];
+
+function validateLogin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        window.location.href = "/index.html";
+    } else {
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = "Credenciales incorrectas";
+        errorMessage.style.display = "block"; // Mostrar el mensaje de error
+    }
+}
